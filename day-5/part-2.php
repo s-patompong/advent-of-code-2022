2fdfe4c653e965830d78c687997ff8d4f1048cf0
Pond <pond@trailercentral.com>
2022-12-05 13:00:36 +0700

    Add day-5 code

---
+++ day-5/part-2.php
@@ -0,0 +1,97 @@
+<?php
+
+class Day5Part2
+{
+    const INPUT_FILE_NAME = 'input.txt';
+
+    const STACK_OPERATION_REGEX = '/move (?<move_amount>[\d]*) from (?<from_stack>[\d]*) to (?<to_stack>[\d]*)/';
+
+    private array $stacks = [];
+
+    public function solve(): void
+    {
+        $input = file_get_contents(__DIR__ . '/' . self::INPUT_FILE_NAME);
+
+        $segments = explode("\n\n", $input);
+
+        $stackSetupLines = explode("\n", $segments[0]);
+        $stackOperationLines = explode("\n", $segments[1]);
+
+        $this->populateStacks($stackSetupLines);
+        $this->performStackOperations($stackOperationLines);
+        $this->printOutput();
+    }
+
+    private function populateStacks(array $stackSetupLines): void
+    {
+        unset($stackSetupLines[count($stackSetupLines) - 1]);
+
+        foreach ($stackSetupLines as $line) {
+            $stacks = str_split($line, 4);
+
+            foreach ($stacks as $index => $stack) {
+                $stackNumber = $index + 1;
+                $stack = trim($stack);
+
+                if (empty($stack)) {
+                    continue;
+                }
+
+                if (!array_key_exists($stackNumber, $this->stacks)) {
+                    $this->stacks[$stackNumber] = [];
+                }
+
+                $this->stacks[$stackNumber][] = $stack[1];
+            }
+        }
+
+        // Revert the creates inside the stack+
+        $this->stacks = array_map(function (array $crates) {
+            return array_reverse($crates);
+        }, $this->stacks);
+    }
+
+    private function performStackOperations(array $stackOperationLines): void
+    {
+        foreach ($stackOperationLines as $line) {
+            if (empty($line)) {
+                continue;
+            }
+
+            preg_match(self::STACK_OPERATION_REGEX, $line, $matches, PREG_OFFSET_CAPTURE);
+
+            $moveAmount = $matches['move_amount'][0];
+            $fromStack = $matches['from_stack'][0];
+            $toStack = $matches['to_stack'][0];
+
+            $temp = [];
+
+            for ($i = 1; $i <= $moveAmount; $i++) {
+                $temp[] = array_pop($this->stacks[$fromStack]);
+            }
+            
+            $temp = array_reverse($temp);
+            
+            foreach ($temp as $crate) {
+                $this->stacks[$toStack][] = $crate;
+            }
+        }
+    }
+
+    private function printOutput(): void
+    {
+        $keys = array_keys($this->stacks);
+
+        sort($keys);
+
+        echo "Message: ";
+
+        foreach ($keys as $key) {
+            echo $this->stacks[$key][count($this->stacks[$key]) - 1];
+        }
+        echo "\n";
+    }
+}
+
+$problem = new Day5Part2();
+$problem->solve();
